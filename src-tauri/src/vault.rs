f2a7e9c21059f84be03c9118910f289e03389289
John Chen <johnhuichen@gmail.com>
2024-12-17 10:45:35 -0500

    Get view vaults working

--- src-tauri/src/vault.rs
+++ src-tauri/src/vault.rs
@@ -14,6 +14,7 @@ use crate::passwords::Passwords;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Vault {
     pub name: String,
+    pub metadata: VaultMetadata,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -75,29 +76,13 @@ impl From<VaultError> for InvokeError {
 }
 
 impl Vault {
-    pub fn new(name: String) -> Result<Self, VaultError> {
-        Self::validate_name(&name)?;
-        Ok(Vault { name })
-    }
-
-    pub fn create(&self, masterkey: &str, config: &Config) -> Result<(), VaultError> {
-        Self::validate_masterkey(masterkey)?;
-        let pwd_path = self.get_pwd_path(config);
-        Self::validate_path_not_exist(&pwd_path)?;
-
-        // Create metadata
-        let now = Local::now();
-        let metadata_path = self.get_meta_path(config);
-        let metadata = VaultMetadata {
-            created_at: now,
-            last_accessed: now,
-        };
-
-        // Save metadata to separate file
-        let metadata = serde_json::to_string(&metadata)?;
-        fs::write(&metadata_path, metadata)?;
+    pub fn new(name: String, masterkey: String, config: &Config) -> Result<Self, VaultError> {
+        Self::validate_masterkey(&masterkey)?;
 
         // Create empty encrypted vault content
+        let pwd_path = Self::get_pwd_path(&name, config);
+        Self::validate_path_not_exist(&pwd_path)?;
+
         let cipher = Cipher::new(masterkey.to_string());
         let file = File::create(pwd_path)?;
         let mut writer = BufWriter::new(file);
@@ -105,15 +90,66 @@ impl Vault {
             .dump(Passwords::random().try_to_vec()?, &mut writer)
             .context(CipherSnafu)?;
 
-        Ok(())
+        // Get or create metadata
+        let metadata = Self::get_metadata(&name, config)?;
+
+        Ok(Vault { name, metadata })
     }
 
-    fn get_pwd_path(&self, config: &Config) -> PathBuf {
-        config.app_data_dir.join(format!("{}.pwd", self.name))
+    pub fn list(config: &Config) -> Result<Vec<Self>, VaultError> {
+        let entries = fs::read_dir(config.app_data_dir.clone())?;
+
+        let mut vaults: Vec<Vault> = entries
+            .flatten()
+            .flat_map(|e| e.file_name().to_str().map(|s| s.to_string()))
+            .filter(|name| name.ends_with(".pwd"))
+            .flat_map(|name| {
+                let end_index = name.len() - 4;
+                let name = String::from(&name[..end_index]);
+                Vault::from_name(name, config).ok()
+            })
+            .collect();
+
+        vaults.sort_by(|a, b| b.metadata.created_at.cmp(&a.metadata.created_at));
+        Ok(vaults)
     }
 
-    fn get_meta_path(&self, config: &Config) -> PathBuf {
-        config.app_data_dir.join(format!("{}.meta", self.name))
+    fn from_name(name: String, config: &Config) -> Result<Self, VaultError> {
+        let metadata = Self::get_metadata(&name, config)?;
+
+        Ok(Vault { name, metadata })
+    }
+
+    fn get_metadata(name: &str, config: &Config) -> Result<VaultMetadata, VaultError> {
+        let pwd_path = Self::get_pwd_path(name, config);
+        Self::validate_path_exist(&pwd_path)?;
+
+        let meta_path = Self::get_meta_path(name, config);
+        if meta_path.exists() {
+            let file = File::open(meta_path)?;
+            let metadata = serde_json::from_reader(file)?;
+
+            Ok(metadata)
+        } else {
+            let now = Local::now();
+            let metadata_path = Self::get_meta_path(name, config);
+            let metadata = VaultMetadata {
+                created_at: now,
+                last_accessed: now,
+            };
+            let metadata_str = serde_json::to_string(&metadata)?;
+            fs::write(&metadata_path, metadata_str)?;
+
+            Ok(metadata)
+        }
+    }
+
+    fn get_pwd_path(name: &str, config: &Config) -> PathBuf {
+        config.app_data_dir.join(format!("{}.pwd", name))
+    }
+
+    fn get_meta_path(name: &str, config: &Config) -> PathBuf {
+        config.app_data_dir.join(format!("{}.meta", name))
     }
 
     fn validate_masterkey(masterkey: &str) -> Result<(), VaultError> {
@@ -150,37 +186,16 @@ impl Vault {
         Ok(())
     }
 
-    // pub fn from_file_name(file_name: &str) -> Option<Self> {
-    //     if !file_name.ends_with(".pwd") {
-    //         return None;
-    //     }
-    //
-    //     let name = file_name.strip_suffix(".pwd")?.to_string();
-    //     let vaults_dir = get_vaults_dir().ok()?;
-    //     let vault_path = vaults_dir.join(file_name);
-    //     let metadata_path = Self::get_metadata_path(&vault_path);
-    //
-    //     None
-    //     // // Read metadata if it exists
-    //     // let metadata = if metadata_path.exists() {
-    //     //     serde_json::from_reader(rdr)
-    //     //     fs::read_to_string(&metadata_path)
-    //     //         .ok()
-    //     //         .and_then(|content| serde_json::from_str::<VaultMetadata>(&content).ok())
-    //     // } else {
-    //     //     None
-    //     // };
-    //     //
-    //     // let last_accessed = metadata
-    //     //     .map(|m| m.last_accessed.format("%Y-%m-%d").to_string())
-    //     //     .unwrap_or_else(|| "Never".to_string());
-    //     //
-    //     // Some(Vault {
-    //     //     id: name.clone(),
-    //     //     name,
-    //     //     last_accessed,
-    //     // })
-    // }
+    fn validate_path_exist(path: &Path) -> Result<(), VaultError> {
+        if !path.exists() {
+            return Err(VaultError::VaultFileNotFound {
+                path: path.to_path_buf(),
+            });
+        }
+
+        Ok(())
+    }
+
     //
     // pub fn update_last_accessed(&self) -> Result<(), VaultError> {
     //     let vaults_dir = get_vaults_dir()?;
